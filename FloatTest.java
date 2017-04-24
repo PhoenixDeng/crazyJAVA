251aa01a4c1c2c1a8de28f1f1ca9c32aab8f989d
phoenixDeng <2583588104@qq.com>
2017-04-24 22:46:34 +0800

    heng

---
+++ FloatTest.java
@@ -0,0 +1,13 @@
+public class FloatTest
+{
+	public static void main(String[] args)
+	{
+	float af = 5.2345556f;
+	System.out.println(af);
+	double c = Double.NEGATIVE_INFINITY;
+    float d = Float.NEGATIVE_INFINITY;
+    System.out.println(c == d);
+    String b1 = true + "";
+    System.out.println(b1);
+    }
+}
